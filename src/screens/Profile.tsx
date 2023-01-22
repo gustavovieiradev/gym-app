e97e0e2428c701ff2156afdb64c6f77f5c5d1a87
Gustavo Vieira <gustavovieiradev@gmail.com>
2023-01-22 17:18:58 -0300

    feat(profile): add inputs passwords

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,6 +1,8 @@
+import Button from "@components/Button";
+import Input from "@components/Input";
 import { ScreenHeader } from "@components/ScreenHeader"
 import { UserPhoto } from "@components/UserPhoto"
-import { Center, ScrollView, Skeleton, Text, VStack } from "native-base"
+import { Center, Heading, ScrollView, Skeleton, Text, VStack } from "native-base"
 import { useState } from "react";
 import { TouchableOpacity } from "react-native";
 
@@ -28,7 +30,22 @@ export const Profile: React.FC = () => {
               Alterar foto
             </Text>
           </TouchableOpacity>
+
+          <Input placeholder="Nome" bg="gray.600" />
+          <Input placeholder="Email" bg="gray.600" isDisabled  />
         </Center>
+
+        <VStack px={10} mt={12} mb={9}>
+          <Heading color="gray.200" fontSize="md" mb={2}>
+            Alterar senha
+          </Heading>
+          <Input placeholder="Senha antiga" bg="gray.600" secureTextEntry />
+          <Input placeholder="Nova senha" bg="gray.600" secureTextEntry />
+          <Input placeholder="Confirme a senha" bg="gray.600" secureTextEntry />
+
+          <Button title="Atualizar" mb={4} />
+        </VStack>
+
       </ScrollView>
     </VStack>
   )
